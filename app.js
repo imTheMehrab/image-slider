887f97a3ead3271f93ccdf9c7e359478865210e8
Mohammad Mehrab Hossain <mohammadmehrabh@gmail.com>
2021-02-14 22:01:38 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,172 @@
+const imagesArea = document.querySelector('.images');
+const gallery = document.querySelector('.gallery');
+const galleryHeader = document.querySelector('.gallery-header');
+const searchBtn = document.getElementById('search-btn');
+const sliderBtn = document.getElementById('create-slider');
+const sliderContainer = document.getElementById('sliders');
+
+// selected image 
+let sliders = [];
+
+
+// If this key doesn't work
+// Find the name in the url and go to their website
+// to create your own api key
+const KEY = '15674931-a9d714b6e9d654524df198e00&q';
+
+// show images 
+const showImages = (images) => {
+  imagesArea.style.display = 'block';
+  gallery.innerHTML = '';
+  // show gallery title
+  galleryHeader.style.display = 'flex';
+  images.forEach(image => {
+    let div = document.createElement('div');
+    div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
+    div.innerHTML = ` 
+    <img class="img-fluid img-thumbnail" 
+    onclick=selectItem(event,"${image.webformatURL}") 
+    src="${image.webformatURL}" alt="${image.tags}">
+    `;
+    toggleLoader(false);
+    gallery.appendChild(div)
+  })
+  searchGoogle();
+}
+
+const getImages = (query) => {
+  toggleLoader(true);
+  fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
+    .then(response => response.json())
+    .then(data => showImages(data.hits))
+    .catch(err => console.log(err))
+}
+
+let slideIndex = 0;
+const selectItem = (event, img) => {
+  let element = event.target;
+  element.classList.add('added');
+
+  let item = sliders.indexOf(img);
+  if (item === -1) {
+    sliders.push(img);
+  } else {
+      // alert('Hey, Already added !')
+      element.classList.toggle('added');
+      sliders.splice(item, 1);
+  }
+}
+var timer
+const createSlider = () => {
+  // check slider image length
+  if (sliders.length < 2) {
+    alert('Select at least 2 images.')
+    return;
+  }
+  // create slider previous next area
+  sliderContainer.innerHTML = '';
+  const prevNext = document.createElement('div');
+  prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
+  prevNext.innerHTML = ` 
+  <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
+  <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
+  `;
+
+  sliderContainer.appendChild(prevNext)
+  document.querySelector('.main').style.display = 'block';
+  // hide image aria
+  imagesArea.style.display = 'none';
+  const duration = document.getElementById('duration').value || 1000;
+  if (duration >= 1000) {
+    sliders.forEach(slide => {
+      let item = document.createElement('div')
+      item.className = "slider-item";
+      item.innerHTML = `<img class="w-100"
+      src="${slide}"
+      alt="">`;
+      sliderContainer.appendChild(item)
+    })
+    changeSlide(0)
+    timer = setInterval(function () {
+      slideIndex++;
+      changeSlide(slideIndex);
+    }, duration);
+  }
+  else {
+    alert(`⚠️For better slide view:⚠️
+⚠️Duration must have to be minimum 1000-ms⚠️`);
+    location.reload();
+  }
+  document.getElementById("googleSearch").classList.add("d-none")
+}
+
+// change slider index 
+const changeItem = index => {
+  changeSlide(slideIndex += index);
+}
+
+// change slide item
+const changeSlide = (index) => {
+
+  const items = document.querySelectorAll('.slider-item');
+  if (index < 0) {
+    slideIndex = items.length - 1
+    index = slideIndex;
+  };
+
+  if (index >= items.length) {
+    index = 0;
+    slideIndex = 0;
+  }
+
+  items.forEach(item => {
+    item.style.display = "none"
+  })
+
+  items[index].style.display = "block"
+}
+
+// adding trigger to "Search" after pressing enter on input field
+document.getElementById('search').addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    searchBtn.click();
+  }
+})
+// adding trigger to "Create Slider" after pressing enter on input field
+document.getElementById('duration').addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    sliderBtn.click();
+  }
+})
+
+// toggle loader
+const toggleLoader = (show) => {
+  if(show){
+    document.getElementById("loading").classList.remove("d-none");
+    document.getElementById("img-container").classList.add("d-none");
+  } else  {
+    document.getElementById("loading").classList.add("d-none");
+    document.getElementById("img-container").classList.remove("d-none");
+  }
+}
+// also search for Google for .....
+const searchGoogle = () => {
+  document.getElementById("googleSearch").classList.remove("d-none");
+  const firstInput = document.getElementById("search").value
+  let footerText = document.createElement("a")
+  footerText.innerText = "Click here"
+  footerText.href = `https://www.google.com/search?q=${firstInput}`
+  document.getElementById("footer-text").appendChild(footerText);
+}
+
+searchBtn.addEventListener('click', function () {
+  document.querySelector('.main').style.display = 'none';
+  clearInterval(timer);
+  const search = document.getElementById('search');
+  getImages(search.value)
+  sliders.length = 0;
+})
+
+sliderBtn.addEventListener('click', function () {
+  createSlider()
+})
